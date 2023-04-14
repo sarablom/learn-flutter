2d6b2b95c040c45ddfdcda20874e78d7498d8a89
sara.blom <sara.blom@medtankengroup.se>
2023-04-14 16:10:03 +0200

    add some expanded examples

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,35 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[900],
       ),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          const Text("Hello world"),
-          ElevatedButton(
-              onPressed: () {},
-              child: const Text("Hej"),
+          Expanded(
+            flex: 3,
+              child: Image.asset("assets/woods.jpg")
           ),
-          Container(
-            color: Colors.cyan,
-            padding: const EdgeInsets.all(15),
-            child: const Text("Hej då"),
-          )
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.cyan,
+              child: const Text("1"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+            padding: const EdgeInsets.all(30),
+            color: Colors.amber,
+            child: const Text("2"),
+          ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+            padding: const EdgeInsets.all(30),
+            color: Colors.green,
+            child: const Text("3"),
+          ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -93,4 +109,55 @@ padding: EdgeInsets.all(30),
 child: Text("Hello"),
 ),*/
 
+// Row
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.end,
+// children: [
+// const Text("Hello world"),
+// ElevatedButton(
+// onPressed: () {},
+// child: const Text("Hej"),
+// ),
+// Container(
+// color: Colors.cyan,
+// padding: const EdgeInsets.all(15),
+// child: const Text("Hej då"),
+// )
+// ],
+// ),
 
+
+//Expanded - like flex
+// Row(
+// children: [
+// Expanded(
+// flex: 3,
+// child: Image.asset("assets/woods.jpg")
+// ),
+// Expanded(
+// flex: 2,
+// child: Container(
+// padding: const EdgeInsets.all(30),
+// color: Colors.cyan,
+// child: const Text("1"),
+// ),
+// ),
+// Expanded(
+// flex: 1,
+// child: Container(
+// padding: const EdgeInsets.all(30),
+// color: Colors.amber,
+// child: const Text("2"),
+// ),
+// ),
+// Expanded(
+// flex: 5,
+// child: Container(
+// padding: const EdgeInsets.all(30),
+// color: Colors.green,
+// child: const Text("3"),
+// ),
+// ),
+// ],
+// ),
